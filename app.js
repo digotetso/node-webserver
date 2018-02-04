b75bd6d5eee29801fc93e250547e6acffc6a1612
Digotetso Matema <dee@Digotetsos-Air.lan>
2018-02-04 11:38:25 +0200

    added start script & dynamic port

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs')
 const fs = require('fs')
 
+const port = process.env.PORT || 3000;
+
 var app =  express();
 
 //tell express which view engine you wanna use
